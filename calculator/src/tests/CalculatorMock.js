8c81cd3040f66fb39bbe2a87c614af1815611584
jonathan <jonathan.stefan@inatel.br>
2024-05-29 18:57:24 -0300

    test: adionando uma classe mock da calculadora

---
+++ calculator/src/tests/CalculatorMock.js
@@ -0,0 +1,59 @@
+class CalculatorMock {
+    constructor() {
+      this.display = '0';
+      this.prevValue = '';
+      this.operator = '';
+    }
+  
+    handleButtonClick(value) {
+      if (value === 'C') {
+        this.display = '0';
+        this.prevValue = '';
+        this.operator = '';
+      } else if (value === '=') {
+        this.calculateResult();
+      } else if (value === '+' || value === '-' || value === '*' || value === '/') {
+        this.prevValue = this.display;
+        this.operator = value;
+        this.display = '0';
+      } else {
+        if (this.display === '0') {
+          this.display = value;
+        } else {
+          this.display += value;
+        }
+      }
+    }
+  
+    calculateResult() {
+      const current = parseFloat(this.display);
+      const previous = parseFloat(this.prevValue);
+      let result = 0;
+      switch (this.operator) {
+        case '+':
+          result = previous + current;
+          break;
+        case '-':
+          result = previous - current;
+          break;
+        case '*':
+          result = previous * current;
+          break;
+        case '/':
+          result = previous / current;
+          break;
+        default:
+          result = current;
+      }
+      this.display = result.toString();
+      this.prevValue = '';
+      this.operator = '';
+    }
+  
+    getDisplay() {
+      return this.display;
+    }
+  }
+  
+  module.exports = CalculatorMock;
+  
